5ccacc4cfd02f64fd287a41da228c16b4ef61f2b
Andrew Kovacs <andrewkov@new-host-5.home>
2012-12-28 12:35:54 -0500

    Organized things a little better into a basic game loop

--- geoquiz.js
+++ geoquiz.js
@@ -1,6 +1,5 @@
 // GeoGame 
-
-var GameMapSVG = function(svg, proj, shapes, onclick) {
+var GameMapSVG = function(svg, proj, shapes, click_hndlr) {
     svg.selectAll("path")
        .data(shapes.features)
        .enter().append("path")
@@ -9,8 +8,33 @@ var GameMapSVG = function(svg, proj, shapes, onclick) {
     svg.on("click", function(d, i) {
         xycoords = d3.mouse(this);
         gpscoords = proj.invert(xycoords);
-        onclick(gpscoords);
+        click_hndlr(gpscoords);
     });
 
     return svg;
 };
+
+var game_loop = function(svg, namebox, cities, shapes) { 
+    alert(cities)
+    if (cities.length == 0) {
+        alert("Its over!");
+        namebox.text("Done!");
+        return;
+    }
+
+    city = cities.features.pop();
+    namebox.text(city.properties.name);
+
+    // for now we alert how many miles to the correct city 
+    
+    // fun lesson here: don't call this "onclick" its a reserved word
+    // and was causing the handler to be called twice!
+    click_hndlr = function(gpscoords) { 
+        d = d3.geo.greatArc().distance({source: city.geometry.coordinates, 
+                                        target: gpscoords});
+        alert(d * 3958.76)
+        game_loop(svg, namebox, cities, shapes);
+    };
+
+    svg = GameMapSVG(svg, d3.geo.albers(), shapes, click_hndlr);
+}
